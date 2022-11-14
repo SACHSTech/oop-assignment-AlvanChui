6003c340270389362b9e7e65c1a5de13a5c11443
AlvanChui <hiufung.chui23@ycdsbk12.ca>
2022-11-14 08:50:14 +0800

    modifying main

--- src/Steam/main.java
+++ src/Steam/main.java
@@ -4,9 +4,29 @@ import java.io.*;
 import Steam.*;
 
 public class main {
-    public static void main(String[] args) {
-        
-        static BufferedReader keyboardReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(" Steam sign in");
+        System.out.print("Enter your username: ");
+        String name = keyboard.readLine();
+        System.out.print("Enter your password: ");
+        String password = keyboard.readLine();
+        System.out.print("Enter your email address: ");
+        String emailAddress = keyboard.readLine();
+        System.out.print("Enter your store location: ");
+        String storeLocation = keyboard.readLine();
+        System.out.print("Enter your real name: ");
+        String realName = keyboard.readLine();
+        System.out.print("Enter your gender: ");
+        char gender = (char)keyboard.read();
+        System.out.print("Enter your birthday: ");
+        String birthday = keyboard.readLine();
+        System.out.print("Do you want to deposit to your Steam wallet?(Y/N) ");
+        double amount = 0.0;
+        if(keyboard.read() == 'Y'){
+            amount += Integer.parseInt(keyboard.readLine());
+        }
+        UserProfile user = new UserProfile(name, realName, password, storeLocation, gender, birthday, emailAddress, amount);
 
         
     }      
